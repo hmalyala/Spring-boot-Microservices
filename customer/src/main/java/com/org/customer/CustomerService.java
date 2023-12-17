a68cb5a21c9e23f5be157c3b807156ae0d4c7d34
hmalyala <m.hemanthkumar27@gmail.com>
2023-12-17 00:55:17 -0500

    RestTemplate

--- customer/src/main/java/com/org/customer/CustomerService.java
+++ customer/src/main/java/com/org/customer/CustomerService.java
@@ -1,9 +1,18 @@
 package com.org.customer;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+@Slf4j
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
     public void registerCustomer(CustomerRegistrationRequest request) {
 
         Customer customer = Customer.builder()
@@ -12,6 +21,14 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .email(request.email())
                 .build();
 
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId());
+        if (fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("fraudster");
+        }
     }
 }
